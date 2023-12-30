0d295e4491c7c8260d20f76914c78d41bae3a538
gxy <1301793728@qq.com>
2023-12-30 16:49:38 +0800

    first  commit  by GXY

---
+++ src/main/java/com/atguigu/learn/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.atguigu.learn.controller;
+
+import com.atguigu.learn.pojo.Employee;
+import com.atguigu.learn.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @RequestMapping(value = "/employee",method = RequestMethod.GET)
+    public String getAllEmployee(Model model){
+        List<Employee> list = employeeService.getAllEmployee();
+        model.addAttribute("list",list);
+        return "employee_list";
+    }
+
+
+}
